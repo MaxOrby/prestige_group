3f48bd26ab4af7c3bd85a291e18eaac213bfc619
Max Orby <webstroymaster@gmail.com>
2020-02-27 14:17:58 +0200

    map-ready

--- js/main.js
+++ js/main.js
@@ -43,3 +43,77 @@ domReady(function() {
 	// run firstWord function	
 	firstWord()
 });
+
+
+
+function initialize() 
+ {
+     var element = document.getElementById('map-canvas');
+     var options = {
+         zoom: 15,
+         center: {lat: 46.505702, lng: 30.630772},
+      };
+
+     var myMap = new google.maps.Map(element, options);
+
+
+     var makers = [
+         {
+         coordinates: {lat: 46.505702, lng: 30.630772},
+         image: '../img/map-marker.png',
+         info:'Prestige Group'
+         },
+
+        /* {
+         coordinates: {lat: 46.462459, lng: 30.729487},
+         image: 'http://afrodita-dance.in.ua/wp-content/uploads/2019/12/mark.png',
+         info:'Афродита Приморский район'
+         },
+
+         {
+         coordinates: {lat: 46.450732, lng: 30.719674},
+         image: 'http://afrodita-dance.in.ua/wp-content/uploads/2019/12/mark.png',
+         info:'Афродита Малиновский район'
+         },*/
+    
+        
+	 ];
+	
+	
+	var mapStyle = [{ "stylers": [{ "saturation": -60 }] }];
+	myMap.setOptions({ styles: mapStyle });
+	
+	
+
+     for(var i=0; i< makers.length; i++)
+     {
+         addMarker(makers[i]);
+     }
+
+     function addMarker(properties){
+        var marker = new google.maps.Marker({
+            position:properties.coordinates,
+            map:myMap,
+        });
+
+        if(properties.image)
+        {
+            marker.setIcon(properties.image);
+        } 
+
+        if(properties.info)
+        {
+            var InfoWindow = new google.maps.InfoWindow({
+                content: properties.info
+            });
+
+            marker.addListener('click', function(){
+                InfoWindow.open(myMap,marker);
+            })
+        } 
+     }
+
+ }
+	
+google.maps.event.addDomListener(window, 'resize', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);	
